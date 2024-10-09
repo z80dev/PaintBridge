1ecdd30d382a3c6c68a1023bd98284987c42315b
z80 <z80@ophy.xyz>
2024-10-09 14:04:55 -0400

    fix: load dotenv early so gunicorn can see it

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
 from .main import app
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
