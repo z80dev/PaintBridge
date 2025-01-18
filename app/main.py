769b70b9d08e8020109b69d42f3f54f1262dc842
z80 <z80@ophy.xyz>
2025-01-18 01:20:23 -0500

    update api to also use NFTBridge class

--- app/main.py
+++ app/main.py
@@ -1,113 +1,97 @@
-#!/usr/bin/env python3
-
 import time
-import dotenv
-dotenv.load_dotenv()
 from flask import Flask, jsonify
-
-from .nft import (
-    airdrop_holders,
-    deploy_1155,
-    get_bridged_address,
-    get_collection_data,
-    deploy_721,
-    get_collection_data_api,
-    get_collection_owner,
-    get_holders_via_api,
-    get_nft_royalty_info,
-    get_onchain_royalty_info,
-    get_token_uris,
-    set_token_uris,
-)
-from .config import get_config
+from .config import env_vars
+from .nft_bridge import NFTBridge
 
 app = Flask(__name__)
-app.config.from_object(get_config())
 
+nft_bridge = NFTBridge(
+    env_vars.DEPLOYER_NAME,
+    env_vars.DEPLOYER_PASSWORD,
+    env_vars.SOURCE_ENDPOINT_ADDRESS,
+    env_vars.TARGET_ENDPOINT_ADDRESS,
+    int(env_vars.EXPECTED_EID),
+    env_vars.FACTORY_ADDRESS,
+    env_vars.BRIDGE_CONTROL_ADDRESS,
+    env_vars.AUTHORIZER_ADDRESS,
+    env_vars.FLASK_ENV,
+    skip_authorizer=True
+)
 
 @app.route("/api/bridge/<param>", methods=["GET"])
 def bridge(param):
-    if bridged_addr := get_bridged_address(param):
-        return jsonify(
-            {
-                "error": "Already bridged",
-                "original_address": param,
-                "bridged_address": bridged_addr,
-            }
-        )
+    if bridged_addr := nft_bridge.get_bridged_address(param):
+        return jsonify({
+            "error": "Already bridged",
+            "original_address": param,
+            "bridged_address": bridged_addr,
+        })
+
     original_address = param
-    holders = get_holders_via_api(original_address)
+    holders = nft_bridge.get_holders_via_api(original_address)
     airdrop_units = list(holders.values())
     is721 = airdrop_units[0].is721
-    base_uri = ""  # will use below for checking if we need to set uris manually
+    base_uri = ""
+
     try:
-        royalty_data = get_nft_royalty_info(original_address)
+        royalty_data = nft_bridge.get_nft_royalty_info(original_address)
     except Exception:
-        royalty_data = get_onchain_royalty_info(original_address)
+        royalty_data = nft_bridge.get_onchain_royalty_info(original_address)
+
     recipient = royalty_data["recipient"]
     fee = royalty_data["fee"]
 
-    collection_data = get_collection_data_api(original_address)
+    collection_data = nft_bridge.get_collection_data_api(original_address)
     if not collection_data.get("verified"):
-        return jsonify(
-            {
-                "error": "Collection not verified",
-                "original_address": original_address,
-            }
-        )
+        return jsonify({
+            "error": "Collection not verified",
+            "original_address": original_address,
+        })
 
     if stats := collection_data.get("stats"):
         total_nfts = int(stats.get("totalNFTs"))
         num_owners = int(stats.get("numOwners"))
-        # fail if the collection has more than 11000 NFTs
+
         if total_nfts > 11000:
-            return jsonify(
-                {
-                    "error": "Collection has too many NFTs",
-                    "original_address": original_address,
-                    "total_nfts": total_nfts,
-                }
-            )
-        # fail if the collection has more than 11000 owners
+            return jsonify({
+                "error": "Collection has too many NFTs",
+                "original_address": original_address,
+                "total_nfts": total_nfts,
+            })
+
         if num_owners > 11000:
-            return jsonify(
-                {
-                    "error": "Collection has too many owners",
-                    "original_address": original_address,
-                    "num_owners": num_owners,
-                }
-            )
-        # fail if the collection has not been sold in 6 months
+            return jsonify({
+                "error": "Collection has too many owners",
+                "original_address": original_address,
+                "num_owners": num_owners,
+            })
+
         last_sale = int(stats.get("timestampLastSale"))
         current_time = int(time.time())
         six_months = 6 * 30 * 24 * 60 * 60
         if current_time - last_sale > six_months:
-            return jsonify(
-                {
-                    "error": "Collection has not been sold in 6 months",
-                    "original_address": original_address,
-                }
-            )
+            return jsonify({
+                "error": "Collection has not been sold in 6 months",
+                "original_address": original_address,
+            })
 
-    original_owner = get_collection_owner(original_address)
+    original_owner = nft_bridge.get_collection_owner(original_address)
     if is721:
-        name, symbol, base_uri, _, extension = get_collection_data(original_address)
-        deployment_tx = deploy_721(
+        name, symbol, base_uri, _, extension = nft_bridge.get_collection_data(original_address)
+        deployment_tx = nft_bridge.deploy_721(
             original_address, original_owner, name, symbol, base_uri, extension, recipient, fee
         )
     else:
-        deployment_tx = deploy_1155(original_address, original_owner, recipient, fee)
+        deployment_tx = nft_bridge.deploy_1155(original_address, original_owner, recipient, fee)
 
-    bridged_address = get_bridged_address(original_address)
+    bridged_address = nft_bridge.get_bridged_address(original_address)
     if not bridged_address:
-        return jsonify(
-            {
-                "error": "Failed to deploy contract to target chain",
-                "original_address": original_address,
-            }
-        )
+        return jsonify({
+            "error": "Failed to deploy contract to target chain",
+            "original_address": original_address,
+        })
 
-    airdrop_txs = airdrop_holders(bridged_address, airdrop_units)
+    airdrop_txs = nft_bridge.airdrop_holders(bridged_address, airdrop_units)
     airdrop_tx_hashes = [tx.txn_hash for tx in airdrop_txs]
 
     response = {
@@ -118,14 +102,12 @@ def bridge(param):
     }
 
     if not is721 or base_uri == "":
-        uris = get_token_uris(original_address, is721=is721)
-        uri_txs = set_token_uris(bridged_address, uris)
+        uris = nft_bridge.get_token_uris(original_address, is721=is721)
+        uri_txs = nft_bridge.set_token_uris(bridged_address, uris)
         response["uri_txs"] = [tx.txn_hash for tx in uri_txs]
     return jsonify(response)
 
-
 @app.route("/api/getBridgedAddress/<param>", methods=["GET"])
 def getBridgedAddress(param):
-    bridged_address = get_bridged_address(param)
+    bridged_address = nft_bridge.get_bridged_address(param)
     return jsonify({"bridged_address": bridged_address})
-
