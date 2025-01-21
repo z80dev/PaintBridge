6fb183a086f68c3961729916e300c5ddccc87b4e
z80 <z80@ophy.xyz>
2025-01-20 20:27:26 -0500

    change ape deploy script to ERC721 mint script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -3,26 +3,26 @@
 from ape import project, accounts
 
 def main():
-deployer = accounts.load("paintbot")
-deployer.set_autosign(True, " ")
-factory = project.NFTFactory.deploy(sender=deployer)
-print(f"Factory deployed at {factory.address}")
+    deployer = accounts.load("paintbot")
+    deployer.set_autosign(True, " ")
+    factory = project.NFTFactory.deploy(sender=deployer)
+    print(f"Factory deployed at {factory.address}")
 
-tx = factory.deployERC721(
-    "0x0000000000000000000000000000000000000000",
-    "NFT Collection",
-    "NFT",
-    "ipfs://baseuri/",
-    ".json",
-    deployer.address,
-    500, # 5% royalty
-    sender=deployer
-)
-collection_address = tx.return_value
-print(f"ERC721 deployed at {collection_address}")
+    tx = factory.deployERC721(
+        "0x0000000000000000000000000000000000000000",
+        "NFT Collection",
+        "NFT",
+        "ipfs://baseuri/",
+        ".json",
+        deployer.address,
+        500, # 5% royalty
+        sender=deployer
+    )
+    collection_address = tx.return_value
+    print(f"ERC721 deployed at {collection_address}")
 
-# mint some tokens
-collection = project.ERC721.at(collection_address)
-tx = collection.bulkAirdrop([(deployer.address, [1, 2, 3])], sender=deployer)
-print("Tokens minted")
-print(f"https://ftmscan.org/tx/{tx.txid}")
+    # mint some tokens
+    collection = project.ERC721.at(collection_address)
+    tx = collection.bulkAirdrop([(deployer.address, [1, 2, 3])], sender=deployer)
+    print("Tokens minted")
+    print(f"https://ftmscan.org/tx/{tx.txn_hash}")
